2f6978d775ef579cc3da37c49aab90b0f096f7f1
Aryannn08 <aryanchauhan082004@gmail.com>
2025-09-23 18:26:03 +0530

    Updated assignments

---
+++ 06-assignment/12-todoList.js
@@ -0,0 +1,39 @@
+let tasks = [];
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    
+    const inputTask = document.getElementById("inputTask");
+    const inputTime = document.getElementById("inputTime");
+
+    if (inputTask.value === '' || inputTime.value === '') {
+        alert("Input field cannot be empty!");
+        return;
+    }
+
+    
+    const obj = {
+        id: Date.now().toString(),
+        name: inputTask.value,
+        time: inputTime.value
+    };
+
+    tasks.push(obj);
+
+    renderTasks();
+
+    inputTask.value = '';
+    inputTime.value = '';
+}
+
+function renderTasks() {
+    listContainer.innerHTML = ''; // clear old list
+    tasks.forEach(task => {
+        const li = document.createElement("li");
+        li.textContent = `${task.name} at ${task.time}`;
+        listContainer.appendChild(li);
+        let btn = document.createElement("button");
+        btn.textContent = "Remove";
+        li.appendChild(btn);
+    });
+}
